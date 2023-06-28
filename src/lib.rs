a63f0f895d687143e772ed641b0a45a86a37b4a9
Chris O'Brian <mobiletread@gmail.com>
2023-06-28 15:25:18 -0700

    add the solution for removing duplicates from a sorted array in place

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod merge_sorted_array;
 pub mod palendrome;
 pub mod remove_an_element;
+pub mod remove_duplicates_from_sorted_array_two;
 pub mod remove_duplicates_sorted_array;
---
+++ src/remove_duplicates_from_sorted_array_two.rs
@@ -0,0 +1,36 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        const CONSECUTIVE_MAX: i32 = 2;
+        let (mut i, mut j, mut count) = (0 as usize, 1 as usize, 1);
+        while j < nums.len() {
+            let identical = nums[i] == nums[j];
+            if identical & (count < CONSECUTIVE_MAX) | !identical {
+                nums[i + 1] = nums[j];
+                i += 1;
+                if identical {
+                    count += 1;
+                } else {
+                    count = 1;
+                }
+            }
+            j += 1;
+        }
+        i += 1;
+        dbg!(nums);
+        i as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case(&mut vec![1,1,1,2,2,3] => 5;"leetcode example 1")]
+    #[test_case(&mut vec![0,0,1,1,1,1,2,3,3] => 7;"leetcode example 2")]
+    fn remove_with_max_of_two(nums: &mut Vec<i32>) -> i32 {
+        Solution::remove_duplicates(nums)
+    }
+}
