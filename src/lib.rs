7031c284b692f2b8aca72dcd97754f50dd57b8ee
Chris O'Brian <mobiletread@gmail.com>
2023-08-10 18:03:07 -0700

    Fixed misspelling of palindrome.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod majority_element_of_an_array;
 pub mod merge_sorted_array;
-pub mod palendrome;
+pub mod palindrome;
 pub mod remove_an_element;
 pub mod remove_duplicates_from_sorted_array_two;
 pub mod remove_duplicates_sorted_array;
+pub mod rotate_array;
